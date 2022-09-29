76c9a7aafd1a1e2ba23d7815589c326d83aa51e5
devjoaogabriel <joao.gabrielmt32@gmail.com>
2022-09-29 17:28:16 -0300

    incluindo efeitos scroll

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,9 +1,14 @@
 const AppEffect = ScrollReveal({
 
     origin: 'left',
-    distance: '60px',
+    distance: '40px',
     duration: 2000,
     delay: 400,
     // reset: true
-    
+
 })
+
+AppEffect.reveal(`.text-figure`)
+AppEffect.reveal(`.title-high, .txt-high`, {origin: 'top'})
+AppEffect.reveal(`.txt-high`, {delay: 700})
+AppEffect.reveal(`.picture-sneakers`, {interval: 100, origin: 'top'})
